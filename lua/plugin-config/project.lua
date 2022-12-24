51a3423ff0a8df5eb9bdb7cc925353d49713e6a7
realtyxxx <841344563@qq.com>
2022-12-24 17:56:39 +0800

    add project  lsp

--- lua/plugin-config/project.lua
+++ lua/plugin-config/project.lua
@@ -13,13 +13,13 @@ end
 vim.g.nvim_tree_respect_buf_cwd = 1
 
 project.setup({
-  detection_methods = { "pattern" },
+  detection_methods = { "lsp", "pattern" },
   patterns = {
-    "README.md",
     "Cargo.toml",
     "package.json",
     ".sln",
     ".git",
+    "README.md",
     "_darcs",
     ".hg",
     ".bzr",
