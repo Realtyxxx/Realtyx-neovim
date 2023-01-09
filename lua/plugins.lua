1ae289cd635d33d6b95f0cab41b0188020e16acb
realtyxxx <841344563@qq.com>
2023-01-09 13:38:08 +0800

    change nvim-tree's owner

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -53,7 +53,7 @@ packer.startup({
     })
     -- nvim-tree
     use({
-      "kyazdani42/nvim-tree.lua",
+      "nvim-tree/nvim-tree.lua",
       requires = "kyazdani42/nvim-web-devicons",
       config = function()
         require("plugin-config.nvim-tree")
