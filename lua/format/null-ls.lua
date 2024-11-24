eaee720b519ee812821187c83184af3978d69487
Realtyxxx <tyx627@icloud.com>
2024-11-24 13:09:06 +0800

    update lazy-lock and null-ls

--- lua/format/null-ls.lua
+++ lua/format/null-ls.lua
@@ -8,92 +8,97 @@ local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 local code_actions = null_ls.builtins.code_actions
 
+local sources = {
+  -- Formatting ---------------------
+  --  brew install shfmt
+  formatting.shfmt,
+  -- StyLua
+  formatting.stylua,
+  -- frontend
+  formatting.prettier.with({ -- 比默认少了 markdown
+    filetypes = {
+      "javascript",
+      "javascriptreact",
+      "typescript",
+      "typescriptreact",
+      "vue",
+      "css",
+      "scss",
+      "less",
+      "html",
+      "json",
+      "yaml",
+      "graphql",
+      "markdown",
+      "tex",
+    },
+    timeout = 10000,
+    prefer_local = "node_modules/.bin",
+  }),
+  -- rustfmt
+  -- rustup component add rustfmt
+  -- formatting.rustfmt,
+  -- Python
+  -- pip install black
+  -- asdf reshim python
+  -- added
+  formatting.clang_format,
+  formatting.autopep8.with({
+    extra_args = { "--ignore E402" },
+  }),
+  -- formatting.black.with({ extra_args = { "--fast" } }),
+  -- formatting.remark_cli,
+  -----------------------------------------------------
+  -- Ruby
+  -- gem install rubocop
+  -- formatting.rubocop,
+  -- json
+  -- npm install -g fixjson
+  -- formatting.fixjson,
+  -- diagnostics.pylint,
+  -- toml
+  -- cargo install taplo-cli
+  -- formatting.taplo,
+  -----------------------------------------------------
+  -- Diagnostics  ---------------------
+  -- diagnostics.eslint.with({
+  --   prefer_local = "node_modules/.bin",
+  -- }),
+
+  -- npm install -g eslint_d
+  -- diagnostics.eslint_d.with({
+  --   prefer_local = "node_modules/.bin",
+  -- }),
+
+  -- diagnostics.markdownlint,
+  -- markdownlint-cli2
+  -- diagnostics.markdownlint.with({
+  --   prefer_local = "node_modules/.bin",
+  --   command = "markdownlint-cli2",
+  --   args = { "$FILENAME", "#node_modules" },
+  -- }),
+
+  -- python
+  -- diagnostics.pylint,
+  -- diagnostics.mypy,
+  -- diagnostics.flake8,
+  -- formatting.latexindent,
+
+  -- code actions ---------------------
+
+  code_actions.gitsigns,
+  -- code_actions.eslint.with({
+  --   prefer_local = "node_modules/.bin",
+  -- }),
+
+  -- npm install -g eslint_d
+  -- code_actions.eslint_d,
+}
+
 null_ls.setup({
   autostart = true,
   debug = true,
-  sources = {
-    -- Formatting ---------------------
-    --  brew install shfmt
-    formatting.shfmt,
-    -- StyLua
-    formatting.stylua,
-    -- frontend
-    formatting.prettier.with({ -- 比默认少了 markdown
-      filetypes = {
-        "javascript",
-        "javascriptreact",
-        "typescript",
-        "typescriptreact",
-        "vue",
-        "css",
-        "scss",
-        "less",
-        "html",
-        "json",
-        "yaml",
-        "graphql",
-        "markdown",
-        "tex",
-      },
-      timeout = 10000,
-      prefer_local = "node_modules/.bin",
-    }),
-    -- rustfmt
-    -- rustup component add rustfmt
-    -- formatting.rustfmt,
-    -- Python
-    -- pip install black
-    -- asdf reshim python
-    -- added
-    formatting.clang_format,
-    formatting.autopep8,
-    -- formatting.black.with({ extra_args = { "--fast" } }),
-    -- formatting.remark_cli,
-    -----------------------------------------------------
-    -- Ruby
-    -- gem install rubocop
-    -- formatting.rubocop,
-    -- json
-    -- npm install -g fixjson
-    -- formatting.fixjson,
-
-    -- toml
-    -- cargo install taplo-cli
-    -- formatting.taplo,
-    -----------------------------------------------------
-    -- Diagnostics  ---------------------
-    -- diagnostics.eslint.with({
-    --   prefer_local = "node_modules/.bin",
-    -- }),
-
-    -- npm install -g eslint_d
-    -- diagnostics.eslint_d.with({
-    --   prefer_local = "node_modules/.bin",
-    -- }),
-
-    -- diagnostics.markdownlint,
-    -- markdownlint-cli2
-    -- diagnostics.markdownlint.with({
-    --   prefer_local = "node_modules/.bin",
-    --   command = "markdownlint-cli2",
-    --   args = { "$FILENAME", "#node_modules" },
-    -- }),
-
-    -- python
-    -- diagnostics.pylint,
-    -- diagnostics.mypy,
-    -- diagnostics.flake8,
-    formatting.latexindent,
-
-    -- code actions ---------------------
-
-    code_actions.gitsigns,
-    -- code_actions.eslint.with({
-    --   prefer_local = "node_modules/.bin",
-    -- }),
-    -- npm install -g eslint_d
-    -- code_actions.eslint_d,
-  },
+  sources = sources,
   -- #{m}: message
   -- #{s}: source name (defaults to null-ls if not specified)
   -- #{c}: code (if available)
